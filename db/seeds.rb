9eb0735b0b73b5ee76b94426e5230c422ddf9ee0
root <root@localhost.localdomain>
2019-01-12 12:41:08 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,23 @@ test_user.password_confirmation = 'test'
 test_user.save!
 
 
+# カテゴリー登録
+Category.create(name: '食事', slug: 'meal', category_type: 'gourmet')
+Category.create(name: 'ディナー', slug: 'dinner', category_type: 'gourmet')
+Category.create(name: 'ランチ', slug: 'lunch', category_type: 'gourmet')
+Category.create(name: 'カフェ', slug: 'cafe', category_type: 'gourmet')
+Category.create(name: '居酒屋・バー', slug: 'bar', category_type: 'gourmet')
+Category.create(name: 'スイーツ', slug: 'sweets', category_type: 'gourmet')
+
+Category.create(name: '美術館', slug: 'art_museum ', category_type: 'art')
+Category.create(name: '博物館', slug: 'museum ', category_type: 'art')
+Category.create(name: '科学館', slug: 'science_museum ', category_type: 'art')
+Category.create(name: '映画館', slug: 'theatre ', category_type: 'art')
+
+Category.create(name: '絶景スポット', slug: 'view_spot ', category_type: 'view_spot')
+Category.create(name: '撮影スポット', slug: 'photo_spot ', category_type: 'view_spot')
+
+Category.create(name: '服屋', slug: 'cloth ', category_type: 'shopping')
+Category.create(name: '雑貨', slug: 'goods ', category_type: 'shopping')
+
+Category.create(name: 'アミューズメント', slug: 'amusement ', category_type: 'other')
