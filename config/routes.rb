589b39c7c24e2a87e4a6879824f6cf6097d3942b
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2019-01-14 06:40:51 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,4 @@ Rails.application.routes.draw do
   get '/location/', to: 'spots#location'
   get '/prefs/', to: 'spots#prefs'
   get '/prefs/:pref', to: 'spots#pref', as: 'pref'
-  
-  get 'search', to: 'spots#search'
-  get 'ajax_select', to: 'spots#ajax_select', as: 'ajax_select'
-  get 'ajax_location', to: 'spots#ajax_location', as: 'ajax_location'
-  get 'ajax_search', to: 'spots#ajax_search', as: 'ajax_search'
-  get 'ajax_category_select', to: 'spots#ajax_category_select', as: 'ajax_category_select'
-  get 'ajax_keyword', to: 'spots#ajax_keyword', as: 'ajax_keyword'
 end
